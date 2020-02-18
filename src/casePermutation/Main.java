d0fd90c01c5b418427abd8e5f5184dd7cc24cbc6
chaninkou <michaelkoulove@yahoo.com>
2020-02-17 18:34:59 -0800

    Great question on dfs(backtrack) and bfs using queue to insert.

--- src/casePermutation/Main.java
+++ src/casePermutation/Main.java
@@ -1,5 +1,14 @@
 package casePermutation;
 
 public class Main {
-
+	public static void main(String[] args) {
+		String S = "a1b2";
+		
+		System.out.println("Input: " + S);
+		
+		FindAllUpperLowerCasePerFunction solution = new FindAllUpperLowerCasePerFunction();
+		
+		System.out.println("Solution: " + solution.letterCasePermutationBFS(S));
+		
+	}
 }
